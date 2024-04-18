eebae2f8154d100717a8e1a2b2ddabc797802992
lehaxe@yahoo.com <lehaxe@yahoo.com>
2024-04-18 12:03:20 +0200

    Blja

--- src/context/provider.tsx
+++ src/context/provider.tsx
@@ -2,9 +2,9 @@ import { Wallet } from "../types/wallet"
 import AccountProvider from "./account/accountContext"
 import WalletProvider from "./wallet/walletContext"
 import { Toaster } from "../components/ui/toaster"
-import { ordinalSafe, unisat, xverse } from "../lib/integrations"
+import { unisat, xverse } from "../lib/integrations"
 
-export const wallets: Wallet[] = [unisat, ordinalSafe, xverse]
+export const wallets: Wallet[] = [unisat, xverse]
 
 export default function OrangeKitProvider({
 	children,
